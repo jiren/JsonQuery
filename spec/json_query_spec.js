9e8eb0d49b78ddeaa566b1cb2e6489cf3c5349f7
sivagollapalli <siva@joshsoftware.com>
2014-06-30 18:34:02 +0530

    Updated specs for $bt operator

--- spec/json_query_spec.js
+++ spec/json_query_spec.js
@@ -1,67 +1,88 @@
-describe("where", function(){
-    var input, expected_movies;
-
-    beforeEach(function(){
-        input = JsonQuery(Movies);
-        
-        expected_movies = [
-            {"name":"Cool Hand Luke","rating":8.2,"director":"Stuart Rosenberg","year":1967,"actor":"Paul Newman"},
-            {"name":"The Graduate","rating":8.1,"director":"Mike Nichols","year":1967,"actor":"Dustin Hoffman"},
-            {"name":"Guess Who's Coming to Dinner","rating":7.7,"director":"Stanley Kramer","year":1967,"actor":"Spencer Tracy"}        ];
-    });
-
-    it("with no operator", function(){
-        actual = input.where({'year': 1967});
-
-        expect(actual.length).toEqual(3)
-        expect(actual[0]).toEqual(expected_movies[0]);
-    });
-
-    it("with $eq operator", function(){
-        actual = input.where({'year.$eq': 1967});
-
-        expect(actual.length).toEqual(3)
-        expect(actual[0]).toEqual(expected_movies[0]);
-    });
-
-    it("with $ne operator", function(){
-        input = JsonQuery(expected_movies);
-        actual = input.where({'rating.$ne': 8.2});
-
-        expect(actual.length).toEqual(2)
-        expect(actual[0]).toEqual(expected_movies[1]);
-        expect(actual[1]).toEqual(expected_movies[2]);
-    });
-});
-
-describe("where", function(){
+describe("JsonQuery", function(){
     var input;
 
     beforeEach(function(){
-      input = JsonQuery(Movies);
+        input = JsonQuery(Movies);
+    });
+    describe("where", function(){
+        var expected_movies;
+
+        beforeEach(function(){
+            expected_movies = [
+               {"name":"Cool Hand Luke","rating":8.2,"director":"Stuart Rosenberg","year":1967,"actor":"Paul Newman"},
+               {"name":"The Graduate","rating":8.1,"director":"Mike Nichols","year":1967,"actor":"Dustin Hoffman"},
+               {"name":"Guess Who's Coming to Dinner","rating":7.7,"director":"Stanley Kramer","year":1967,"actor":"Spencer Tracy"}          ];
+        });
+
+        it("with no operator", function(){
+            actual = input.where({'year': 1967});
+
+            expect(actual.length).toEqual(3)
+            expect(actual[0]).toEqual(expected_movies[0]);
+        });
+
+        it("with $eq operator", function(){
+            actual = input.where({'year.$eq': 1967});
+
+            expect(actual.length).toEqual(3)
+            expect(actual[0]).toEqual(expected_movies[0]);
+        });
+
+        it("with $ne operator", function(){
+            input = JsonQuery(expected_movies);
+            actual = input.where({'rating.$ne': 8.2});
+
+            expect(actual.length).toEqual(2)
+            expect(actual[0]).toEqual(expected_movies[1]);
+        expect(actual[1]).toEqual(expected_movies[2]);
+        });
     });
 
-    it("with $li operator", function(){
-        actual = input.where({'name.$li': 'Terminator'});
+    describe("where", function(){
+        it("with $li operator", function(){
+            actual = input.where({'name.$li': 'Terminator'});
 
-        expect(actual.length).toEqual(2)
-        expect(actual[0].name).toEqual("Terminator 2: Judgment Day");
+            expect(actual.length).toEqual(2)
+            expect(actual[0].name).toEqual("Terminator 2: Judgment Day");
         expect(actual[1].name).toEqual("The Terminator");
-    });
+        });
 
-    it("with $li operator with regex", function(){
-        actual = input.where({'name.$li': /[a-zA-Z]+ [0-9]+/});
+        it("with $li operator with regex", function(){
+            actual = input.where({'name.$li': /[a-zA-Z]+ [0-9]+/});
 
-        expect(actual.length).toEqual(2)
-        expect(actual[0].name).toEqual("Terminator 2: Judgment Day");
+            expect(actual.length).toEqual(2)
+            expect(actual[0].name).toEqual("Terminator 2: Judgment Day");
         expect(actual[1].name).toEqual("4 Months, 3 Weeks and 2 Days");
+        });
+
+        it("with $li without case sensitiveness", function(){
+            actual = input.where({'name.$li': /terminator/i});
+
+            expect(actual.length).toEqual(2)
+            expect(actual[0].name).toEqual("Terminator 2: Judgment Day");
+            expect(actual[1].name).toEqual("The Terminator");
+        });
     });
 
-    it("with $li without case sensitiveness", function(){
-       actual = input.where({'name.$li': /terminator/i});
+    describe("where", function(){
+        it("with $bt operator", function(){
+            actual = input.where({'year.$bt': [1991, 1993]});
 
-       expect(actual.length).toEqual(2)
-       expect(actual[0].name).toEqual("Terminator 2: Judgment Day");
-       expect(actual[1].name).toEqual("The Terminator");
+            expect(actual.length).toEqual(1)
+            expect(actual[0]).toEqual({"name":"The Last of the Mohicans","rating":7.7,"director":"Michael Mann","year":1992,"actor":"Daniel Day-Lewis"});
+        });
+
+        it("$bt operator with improper range", function(){
+            actual = input.where({'year.$bt': [9.1, 9.3]});
+
+            expect(actual.length).toEqual(0)
+        });
+
+        it("$bt operator with float range", function(){
+            actual = input.where({'rating.$bt': [8.5, 8.7]});
+
+            expect(actual.length).toEqual(1)
+            expect(actual[0]).toEqual({"name":"Terminator 2: Judgment Day","rating":8.6,"director":"James Cameron","year":1991,"actor":"Arnold Schwarzenegger"});
+        });
     });
 });
